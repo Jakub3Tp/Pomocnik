469c0e0227b5aa4ef528432ca7214e9039e93a25
kubix <jakub.gola2@teb.edu.pl>
2025-05-11 19:27:13 +0200

    11.05.2025

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtWidgets import QDialog, QApplication
+from PyQt6.QtWidgets import QDialog, QApplication, QFileDialog
 
 from layout import Ui_Dialog
 
@@ -10,12 +10,16 @@ class MyForm(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.load.clicked.connect(self.checkNumber)
         self.show()
 
-    def getNumber(self):
-
+    def checkNumber(self):
+        file_name = QFileDialog().getOpenFileName(self, "Wybierz", ".", "*.txt")
+        if file_name != '':
+            with open(file_name, "r") as file:
+                file.write(self.ui.numbers.toPlainText())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MyForm
+    window = MyForm()
     sys.exit(app.exec())
